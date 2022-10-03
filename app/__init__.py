75292f36cfdbcdbe92f619abf82b96124795321f
jangjue <jangjue98@gmail.com>
2022-10-04 06:08:51 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,29 +6,32 @@ from flask_bootstrap import Bootstrap
 from pymysql import connections
 import os
 import boto3
-from config import app_config
+import MySQLdb
+from sqlalchemy import create_engine
+from config import *
 
 # Variable initialization
-# db = SQLAlchemy()
+db = SQLAlchemy()
 login_manager = LoginManager()
-
+#    bucket = custombucket
+#    region = customregion
+    
+    host=customhost,
+    port=3306,
+    user=customuser,
+    password=custompass,
+    db=customdb       
+ 
+engine = create_engine("mysql://customuser:custompass@customhost:port/db")
 def create_app(config_name):
     # Initialize the app
     app = Flask(__name__, instance_relative_config = True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     
-    bucket = custombucket
-    region = customregion
 
-    db = connections.Connection(
-    host=customhost,
-    port=3306,
-    user=customuser,
-    password=custompass,
-    db=customdb
 
-)
+
     
     Bootstrap(app)
     db.init_app(app)
