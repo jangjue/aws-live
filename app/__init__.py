a5833b0ac0bdf8c1cd988efa15f594c39b734a2f
jangjue <jangjue98@gmail.com>
2022-10-04 03:55:03 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_bootstrap import Bootstrap
-
+from pymysql import connections
+import os
+import boto3
 from config import app_config
 
 # Variable initialization
